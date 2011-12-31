6b9fe1e8f167dee0069e66aa6f21d8923155adb5
Stefane Fermigier <sf@fermigier.com>
2011-12-31 12:13:00 +0100

    Fix test.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,45 +1,28 @@
 import tempfile
 import os
-import sys
 import pwd
 
 from tentakel.config import ConfigBase
 
+
 def test_config():
-  failures = 0
-  print "self testing..."
-
-  print "### instantiate ConfigBase:"
   c1 = ConfigBase()
-  if isinstance(c1, ConfigBase):
-    print "OK"
-  else: failures += 1
+  assert isinstance(c1, ConfigBase)
 
-  print "### load example config:"
-  try:
-    f = open("../../tentakel.conf.example")
-    c1.load(f)
-    f.close()
-    print "OK"
-  except:
-    print "-> failed <-"
-    failures += 1
+  # load example config
+  f = open("doc/tentakel.conf.example")
+  c1.load(f)
+  f.close()
 
-  print "### regenerate ourselves from a dump:"
+  # regenerate ourselves from a dump
   tmp = tempfile.TemporaryFile()
   c1.dump(tmp)
   c2 = ConfigBase()
-  tmp.seek(0,0)
+  tmp.seek(0, 0)
   c2.load(tmp)
-  if c1 == c2:
-    print "OK"
-  else:
-    print "-> failed <-"
-    failures += 1
-  del c2
   tmp.close()
 
-  print "### ugly config syntax:"
+  # ugly config syntax
   uglyconfig = [
     '# all of these should work:\n',
     'set method="ssh"\n',
@@ -51,33 +34,16 @@ def test_config():
   ]
   tmp = tempfile.TemporaryFile()
   tmp.writelines(uglyconfig)
-  tmp.seek(0,0)
+  tmp.seek(0, 0)
   c3 = ConfigBase()
-  try:
-    c3.load(tmp)
-    print "OK"
-  except:
-    print "-> failed <-"
-    failures += 1
-  del c3
+  c3.load(tmp)
   tmp.close()
 
-  print "### read parameter:"
+  # read parameter
   user1 = pwd.getpwuid(os.geteuid())[0]
   user2 = c1.getParam("user")
-  if user1 == user2:
-    print "OK"
-  else:
-    print "-> failed <-"
-    print "read", user2, "but should be", user1
-    failures += 1
+  assert user1 == user2
 
-  if failures:
-    print "self test: encountered", failures, "failures."
-    sys.exit(1)
-  else:
-    print "self test: no failures."
-    sys.exit(0)
 
 if __name__ == "__main__":
   test_config()
